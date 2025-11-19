34cc0ef39d5eb41d2f7a47fff138d2bc9284d227
mres42 <mario.github@protonmail.com>
2025-11-19 05:02:25 -0300

    map to obj

---
+++ 1-array-method-exercises/2-map-to-objects.js
@@ -0,0 +1,19 @@
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+let usersMapped = users.map((user) => ({
+    name: user.name + " " + user.surname,
+    if: user.id
+}))
+
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+
+alert( usersMapped[0].id )
+alert( usersMapped[0].fullName )
