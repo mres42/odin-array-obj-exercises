92f58c4d1412d25eee5f3c8737bafd0918f9130f
mres42 <mario.github@protonmail.com>
2025-11-19 05:07:00 -0300

    sort by age

---
+++ 1-array-method-exercises/3-sort-users-by-age.js
@@ -0,0 +1,17 @@
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [ pete, john, mary ];
+
+// my solution
+function sortByAge(arr) {
+    arr.sort((x, y) => x.age - y.age);
+}
+
+sortByAge(arr);
+
+// now: [john, mary, pete]
+alert(arr[0].name); // John
+alert(arr[1].name); // Mary
+alert(arr[2].name); // Pete
